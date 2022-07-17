c600acf1cb62c3ec32508f281c64eaecba642ffc
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2022-07-17 15:56:39 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,18 @@
+export const restaurant = [
+    {
+        id: 0,
+        name: 'Taco Bell'
+    },
+    {
+        id: 1,
+        name: 'KFC'
+    },
+    {
+        id: 2,
+        name: 'McDonalds'
+    },
+    {
+        id: 3,
+        name: 'Italiano'
+    }
+]
